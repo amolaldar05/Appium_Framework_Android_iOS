a5e4e7ab583156901d492f5bc96ec583806a0d43
Amol Aldar <amolaldar05@gmail.com>
2025-07-24 23:46:17 +0400

    modified simple test into pageObjects

--- src/test/java/org/android/PageObjects/FormPage.java
+++ src/test/java/org/android/PageObjects/FormPage.java
@@ -3,37 +3,59 @@ package org.android.PageObjects;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.android.utils.AndroidActions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
 
-public class FormPage {
+public class FormPage extends AndroidActions {
     @AndroidFindBy(id = "com.androidsample.generalstore:id/spinnerCountry")
-    public WebElement countryDropdown;
+    private WebElement countryDropdown;
     @AndroidFindBy(className = "android.widget.EditText")
-    public WebElement nameField;
+    private WebElement nameField;
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/radioFMale")
+    private WebElement maleRadioButton;
     @AndroidFindBy(id = "com.androidsample.generalstore:id/radioFemale")
-    public WebElement femaleRadioButton;
+    private WebElement femaleRadioButton;
     @AndroidFindBy(id = "com.androidsample.generalstore:id/btnLetsShop")
-    public WebElement letsShopButton;
+    private WebElement letsShopButton;
     @AndroidFindBy(xpath = "(//android.widget.Toast)[1]")
-    public WebElement toastMessage;
+    private WebElement toastMessage;
 
     public FormPage(AndroidDriver driver) {
+        super(driver);
+        this.driver=driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
     }
 
+
     public void selectCountry(String countryName) {
         countryDropdown.click();
+        WebElement country=scrollUptoUsingUIAutomator2(countryName);
+        country.click();
 
     }
-    public void fillForm(String countryName, String userName) {
-        countryDropdown.click();
-        // Scrolling and clicking to be done in test using driver
-        nameField.sendKeys(userName);
-        femaleRadioButton.click();
+
+    public void enterName(String name){
+        nameField.sendKeys(name);
+    }
+    public void selectGender(){
+       if (!maleRadioButton.isSelected()){
+    maleRadioButton.click();
+    }else{
+    femaleRadioButton.click();
+         }
+    }
+
+    public void clickShopBtn(){
         letsShopButton.click();
+    }
+    public void fillForm(String countryName, String userName) {
+        selectCountry(countryName);
+        enterName(userName);
+        selectGender();
+        clickShopBtn();
     }
 }
